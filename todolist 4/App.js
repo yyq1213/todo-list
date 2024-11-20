69e52840c596ceb62bb32f00c9362b6e7d5e7e53
yyq1213 <2352814692@qq.com>
2024-11-20 17:58:53 +0800

    Add files via upload

---
+++ todolist 4/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FloatingWindow from './FloatingWindow';
+
+function App() {
+  return (
+    <div className="App">
+      <FloatingWindow />
+    </div>
+  );
+}
+
+export default App; // 确保这里使用了默认导出
